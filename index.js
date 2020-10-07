69012e38354cc21c9685cc1f1050ec9c4494a505
sebastian espeset <sebastian.c.espeset@gmail.com>
2020-10-07 16:13:44 -0700

    all done

--- index.js
+++ index.js
@@ -173,8 +173,16 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-    /*your code here*/
+function filterByWord(iceCreamList, flavor){
+    let specificFlavors=[]
+    for (let i=0; i < iceCreamList.length;i++){
+        if(iceCreamList[i].includes(flavor)){
+            specificFlavors.push(iceCreamList[i]);
+        }
+        else{
+            continue;
+        }
+    }return specificFlavors;
 }
 
 
